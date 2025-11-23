4da6d2ae5178f43306ff0bcb314b0eb4ded83706
Brahim-BT <baibbat.brahim@gmail.com>
2025-11-23 18:16:55 +0100

    fix: Correct user reference to owner in BookTransactionHistoryRepository and BookSpecification

--- src/main/java/com/brahim/book_network_api/specification/BookSpecification.java
+++ src/main/java/com/brahim/book_network_api/specification/BookSpecification.java
@@ -7,6 +7,6 @@ import com.brahim.book_network_api.model.Book;
 public class BookSpecification {
 
     public static Specification<Book> withOwnerId(Long ownerId) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("user").get("id"), ownerId);
+        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("owner").get("id"), ownerId);
     }
 }
